1b5b3ae43b35e7e318159b2bfd5826df1f8b357c
cympfh <cympfh@gmail.com>
2020-06-07 12:58:37 +0900

    structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate structopt;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    verbose: bool,
+}
+
 fn main() {
+    let opt = Opt::from_args();
     println!("Hello, world! {}", rand::random::<u32>());
+    println!("verbose: {:?}", opt.verbose);
 }
