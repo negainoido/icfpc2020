65e2ee431ed204231a37ee46184697b3b5764bd0
tikuta <tikuta@linux>
2020-06-07 20:43:44 +0900

    log

--- src/bin/solver1.rs
+++ src/bin/solver1.rs
@@ -1,8 +1,32 @@
+extern crate structopt;
+use structopt::StructOpt;
+
 extern crate icfpc2020;
 use icfpc2020::ai::AI;
+use icfpc2020::opt::{common_init, CommonOpt};
+
+#[macro_use]
+extern crate log;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(flatten)]
+    common: CommonOpt,
+    #[structopt(short, long, default_value = "3")]
+    num: usize,
+}
 
 fn main() {
+    common_init();
+
+    let opt = Opt::from_args();
     let ai = AI();
+    println!("{:?}", opt);
+    println!("{:?}", ai);
+
     println!("This is solver1.");
-    println!("{:?} {}", ai, rand::random::<u32>())
+    println!("{:?} {}", ai, rand::random::<u32>());
+    info!("This is info log");
+    warn!("This is warning log");
+    error!("This is error log");
 }
--- src/opt.rs
+++ src/opt.rs
@@ -1,8 +1,25 @@
+extern crate env_logger;
+
 extern crate structopt;
 use structopt::StructOpt;
+
+extern crate log;
+use log::LevelFilter;
 
 #[derive(Debug, StructOpt)]
 pub struct CommonOpt {
     #[structopt(short, long)]
     verbose: bool,
+}
+
+pub fn common_init() {
+    let opt = CommonOpt::from_args();
+
+    if opt.verbose {
+        env_logger::Builder::new()
+            .filter_level(LevelFilter::Info)
+            .init();
+    } else {
+        env_logger::init();
+    }
 }
