31686382808a6aa7e9c9bca7c08c524a6701627e
cympfh <cympfh@gmail.com>
2020-06-20 01:26:45 +0900

    Add sample test code

--- solver/src/ai.rs
+++ solver/src/ai.rs
@@ -4,4 +4,18 @@ impl AI {
     pub fn new() -> Self {
         AI()
     }
+    pub fn square(self, n: u128) -> u128 {
+        n * n
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AI;
+
+    #[test]
+    fn it_works() {
+        let ai = AI::new();
+        assert_eq!(ai.square(2), 4);
+    }
 }
