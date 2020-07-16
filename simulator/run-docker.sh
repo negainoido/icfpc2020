3effbbd0e4a825086120ae26a426b9bd25e04a28
garasubo <garasubo@gmail.com>
2020-07-16 23:23:33 +0900

    Improve docker (#45)
    * remove build arg
    
    * add CODEOWNER
    
    * add gitignore
    
    * remove unnecessary option
    
    * update run-docker
    
    * update cloudbuild.yaml
    
    * fix build
    
    * tweak

--- simulator/run-docker.sh
+++ simulator/run-docker.sh
@@ -2,24 +2,28 @@
 
 set -e
 
+if [[ $# -ne 3 ]]; then
+    echo "
+Usage:
 
-if [[ -z $1 ]]; then
-  echo "Please set input file"
-  exit 1
-fi
+$ $0 <image_id> <input file> <solution file>
 
-if [[ -z $2 ]]; then
-  echo "Please set solution file"
-  exit 1
+e.g.
+
+$ $0 simulator:latest ./inputs/input.txt ./solutions/solution.txt
+
+" 1>&2
+    exit 1
 fi
 
 script_path=$(readlink -f "$0")
-input_file=$(readlink -f "$1")
-solution_file=$(readlink -f "$2")
+input_file=$(readlink -f "$2")
+solution_file=$(readlink -f "$3")
 inputs_dir=$(dirname "$input_file")
 solutions_dir=$(dirname "$solution_file")
 
 cd $(dirname "$script_path")
+cp "$input_file" ./tmp/input.txt
+cp "$solution_file" ./tmp/solution.txt
 
-IMAGE_ID=$(docker build -q . --build-arg input="$(basename \"$input_file\")" --build-arg solution="$(basename \"$solution_file\")")
-docker run -v /dev/shm:/dev/shm -v "$inputs_dir":/app/inputs -v "$solutions_dir":/app/solutions $IMAGE_ID
+docker run -v /dev/shm:/dev/shm -v "$(pwd)/tmp":/app/files "$1"
