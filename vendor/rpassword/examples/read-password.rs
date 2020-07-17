e7dfb62c9a7f416cca9de9fa90741a823ece0f54
garasubo <garasubo@gmail.com>
2020-07-17 23:47:41 +0900

    clean up unnecessary files (#59)
    * remove unnecessary files
    
    * a
    
    * a

--- vendor/rpassword/examples/read-password.rs
+++
@@ -1,8 +0,0 @@
-//! This example demonstrates how to read a password from STDIN.
-
-extern crate rpassword;
-
-fn main() {
-    let pass = rpassword::read_password().unwrap();
-    println!("Password: {}", pass);
-}
