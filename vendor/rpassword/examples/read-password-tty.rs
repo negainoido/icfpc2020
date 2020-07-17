a624f343a2ad5dc0bbc200feba3f449aa520e3b7
garasubo <garasubo@gmail.com>
2020-07-17 23:25:41 +0900

    remove unnecessary files

--- vendor/rpassword/examples/read-password-tty.rs
+++
@@ -1,8 +0,0 @@
-//! This example demonstrates how to read a password from the tty.
-
-extern crate rpassword;
-
-fn main() {
-    let pass = rpassword::read_password_from_tty(Some("Password: ")).unwrap();
-    println!("Password: {}", pass);
-}
--- vendor/secret-service/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-## Version 1.1.0
-- hkdf dependency updated.
-- Error properly returned when a collection is not found.
