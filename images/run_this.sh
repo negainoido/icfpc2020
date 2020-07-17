0b6af67e83a89f1806d8bfb37b0667c413d630f5
tikuta <tikuta@linux>
2020-07-18 00:02:45 +0900

    update run_this.sh

--- images/run_this.sh
+++ images/run_this.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 
-for i in {1..15}
+for i in {1..42}
 do
     wget -q "https://message-from-space.readthedocs.io/en/latest/_images/message${i}.png" \
          -O "message${i}.png"
