107a19f7a9f4a80fa3d9454d9312a08800b6df6a
tikuta <tikuta@linux>
2020-07-17 21:45:55 +0900

    run build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+# https://github.com/icfpcontest2020/starterkit-rust/blob/4afd3ffceacb3000fe7321c40deb5dd3aa746a5b/build.sh
 
 # use --release later if necessary
-cargo build
+cargo build --offline
--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+
+# https://github.com/icfpcontest2020/starterkit-rust/blob/4afd3ffceacb3000fe7321c40deb5dd3aa746a5b/run.sh
+
+./target/debug/cympfh "$@" || echo "run error code: $?"
