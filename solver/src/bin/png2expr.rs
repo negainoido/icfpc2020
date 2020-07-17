14ac8dccfb6a5808a806c66946c8ae0694424738
Takuto Ikuta <atetubou@gmail.com>
2020-07-18 04:20:46 +0900

    remove image (#75)
    Co-authored-by: tikuta <tikuta@linux>

--- solver/src/bin/png2expr.rs
+++ solver/src/bin/png2expr.rs
@@ -1,17 +1,16 @@
-use image::DynamicImage;
+// use image::DynamicImage;
 
 use structopt::StructOpt;
 
-use icfpc2020::decode;
+// use icfpc2020::decode;
 use icfpc2020::opt::{common_init, CommonOpt};
 
 #[derive(StructOpt)]
 struct Opt {
     #[structopt(flatten)]
     common: CommonOpt,
-
-    #[structopt(short, long, parse(try_from_str= image::open))]
-    input: DynamicImage,
+    // #[structopt(short, long, parse(try_from_str= image::open))]
+    // input: DynamicImage,
     //
     // #[structopt(short, long)]
     // output: String,
@@ -20,9 +19,9 @@ struct Opt {
 fn main() {
     let opt: Opt = Opt::from_args();
     common_init(&opt.common);
-    let expr = decode::png2expr(opt.input);
+    // let expr = decode::png2expr(opt.input);
 
-    for e in expr {
-        println!("{:?}", e)
-    }
+    // for e in expr {
+    //     println!("{:?}", e)
+    // }
 }
