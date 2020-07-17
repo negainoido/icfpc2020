213faf3edf424c48b7e187db576598d507c59d59
tailed0 <33681603+tailed0@users.noreply.github.com>
2020-07-18 04:21:48 +0900

    Demodulate (#73)
    * initial commit
    
    * small tool for demodulating
    
    * bug fix
    
    * from argument
    
    Co-authored-by: Shuichi Hirahara <shuichi@DESKTOP-LJHTRQL.localdomain>
    Co-authored-by: Shuichi Hirahara <s_hirahara@nii.ac.jp>

---
+++ tools/demodulate.rb
@@ -0,0 +1,51 @@
+# example: ruby demodulate.rb 110110000111011111100001001111110101000000
+def demodulate(s)
+	prefix = s.shift + s.shift
+
+	res = []
+	case prefix
+	when "00"
+		res << "nil"
+	when "11"
+		res += demodulate(s)
+		res += demodulate(s)
+	else
+		if prefix == "01"
+			sign = 1 
+		else
+			sign = -1
+		end
+		len = 0
+		while s[0] != "0"
+			len += 1
+			s.shift
+		end
+		s.shift
+		len *= 4
+		num =  s[0..len-1].join("").to_i(2)
+		res << sign * num
+		len.times { s.shift }
+	end
+	res
+end
+p demodulate(ARGV[0].split(""))
+=begin
+a = "110110000111011111100001001111110101000000".split("")
+#a = "1101000".split("")
+p demodulate(a)
+p a
+
+a = "110110000111011111100001001111110100110000".split("")
+p demodulate(a)
+p a
+
+p demodulate("110110000111011111100001001101111010100000".split(""))
+
+p demodulate("110110000111011111100001001101010110011100".split(""))
+
+
+p demodulate("110110000111011111100001001101010110011100".split(""))
+
+p demodulate("110110000111011111100001001101001001111000".split(""))
+=end
+
