960772e410830922ba27dce3e065eb3d41517af1
Takuto Ikuta <atetubou@gmail.com>
2020-07-19 01:41:14 +0900

    remove solver1 (#128)
    * remove solver1
    
    * remove unused
    
    Co-authored-by: tikuta <tikuta@linux>

--- solver/tests/test.rs
+++ solver/tests/test.rs
@@ -1,54 +1,11 @@
 use glob::glob;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::Read;
+use std::path::PathBuf;
 
 use assert_cmd::Command;
 
 use regex::Regex;
-use tempfile::tempdir;
-
-#[test]
-fn basic() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let dir = tempdir()?;
-
-    let input = dir.path().join("input.txt");
-    let output = dir.path().join("output.txt");
-    {
-        let mut file = File::create(&input)?;
-        write!(file, "test")?;
-    }
-
-    let mut cmd = Command::cargo_bin("solver1")?;
-    cmd.args(&[
-        "--input",
-        input.to_str().unwrap(),
-        "--output",
-        output.to_str().unwrap(),
-    ]);
-    cmd.assert().success();
-
-    assert!(Path::new(&output).exists());
-
-    let mut cmd = Command::cargo_bin("solver1")?;
-    cmd.args(&[
-        "--input",
-        input.to_str().unwrap(),
-        "--output",
-        output.to_str().unwrap(),
-    ]);
-    cmd.arg("--verbose").assert().success();
-
-    let mut cmd = Command::cargo_bin("solver1")?;
-    cmd.args(&[
-        "--input",
-        input.to_str().unwrap(),
-        "--output",
-        output.to_str().unwrap(),
-    ]);
-    cmd.arg("--num").assert().success();
-    Ok(())
-}
 
 #[test]
 fn test_solver() -> std::result::Result<(), Box<dyn std::error::Error>> {
