b1ddf88d8c377f1fc5f488927d38d9c16bf74e18
Shuichi Hirahara <s_hirahara@nii.ac.jp>
2020-07-18 23:09:46 +0900

    interactive click

--- interpreter/vis.rb
+++ interpreter/vis.rb
@@ -15,44 +15,74 @@ def write_out(image, rgb, opaq)
 		@pixels[ [x, y] ] = rgb.map {|c| (c * opaq).to_i}
 	end
 end
- 
-autotaker = IO.popen(["cabal", "new-exec", "interpreter"], "r+")
-autotaker.puts File.open("galaxy.txt").read()
-autotaker.close_write
 
-#lines = system("cabal new-exec interpreter  <galaxy.txt")
-lines = autotaker.read()
-autotaker.close
-#lines = File.open("data").read()
-puts lines
-dataascode = lines =~ /DataAsCode: (.*)/ ? $1 : nil
-images = read_image_from_string(lines)
+def exec_autotaker(point, data = "nil")
+	galaxy = File.open("galaxy.txt").read()
+	galaxy.gsub!(/^(:2000 = ap ap cons) (.*)$/, "\\1 #{point[0]} #{point[1]}")
+	galaxy.gsub!(/^(:2001 = )(.*)$/, "\\1#{data}")
+	puts "point: #{point[0]} #{point[1]}"
+	puts "data: #{data}"
 
-#plot = IO.popen("gnuplot", "r+")
-plot = IO.popen("gnuplot", "r+", :err => [:child, :out])
+	lines = IO.popen(["cabal", "new-exec", "interpreter"], "r+") do |autotaker|
+		autotaker.puts galaxy
+		autotaker.close_write
 
-images.length.times do |i|
-	plot.puts "$image#{i} << EOD"
-	images[i].each do |x, y|
-		plot.puts "#{x} #{y}"
-	end
-	plot.puts "EOD"
-end
-
-data = images.length.times.map {|i| "$image#{i}"}.join(",")
-plot.puts "plot #{data}"
-
-
-plot.puts "set mouse verbose"
-
-while l = plot.gets
-	puts "gnuplot: #{l}"
-	if l =~ /put `\s*(-?[-0-9\.]*),\s*(-?[0-9\.]*)' to clipboard\./
-		x = $1.to_f
-		y = $2.to_f
-		p [x, y]
+		autotaker.read()
 	end
 end
+
+def plot_and_interact(lines)
+	images = read_image_from_string(lines)
+
+	#plot = IO.popen("gnuplot", "r+")
+	plot = IO.popen("gnuplot", "r+", :err => [:child, :out])
+
+	images.length.times do |i|
+		plot.puts "$image#{i} << EOD"
+		images[i].each do |x, y|
+			plot.puts "#{x} #{y}"
+		end
+		plot.puts "EOD"
+	end
+
+	data = images.length.times.map {|i| "$image#{i}"}.join(",")
+	plot.puts "plot #{data}"
+
+
+	plot.puts "set mouse verbose"
+
+	while l = plot.gets
+		puts "gnuplot: #{l}"
+		if l =~ /put `\s*(-?[-0-9\.]*),\s*(-?[0-9\.]*)' to clipboard\./
+			x = $1.to_f.round
+			y = $2.to_f.round
+			puts "Clicked: #{x} #{y}"
+			return [x, y]
+		end
+	end
+end
+
+next_point = [0, 0]
+data = "nil"
+
+#next_point = [1, 4]
+#data = "ap ap cons 2 ap ap cons ap ap cons 1 ap ap cons -1 nil ap ap cons 0 ap ap cons nil nil"
+
+
+while true
+	lines = exec_autotaker(next_point, data)
+	puts "### autotaker"
+	puts lines
+
+	data = lines =~ /DataAsCode: (.*)/ ? $1 : nil
+	if !data
+		$stderr.puts "!!!Warning!!!:  We could not find the next data"
+		data = "nil"
+	end
+
+	next_point = plot_and_interact(lines)
+end
+
 
 exit
 
