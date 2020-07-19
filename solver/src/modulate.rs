914f1b709e35994c3129b5a726107d07e1488f30
Takuto Ikuta <atetubou@gmail.com>
2020-07-19 15:46:52 +0900

    find (#162)
    * use find
    
    * refactor
    
    Co-authored-by: tikuta <tikuta@linux>

--- solver/src/modulate.rs
+++ solver/src/modulate.rs
@@ -9,28 +9,22 @@ pub enum List {
 }
 
 fn do_demodulate(a: &str) -> (&str, List) {
-    let prefix = &a[..2];
+    let (prefix, a) = (&a[..2], &a[2..]);
+
     if prefix == "11" {
-        let (a, car) = do_demodulate(&a[2..]);
+        let (a, car) = do_demodulate(a);
         let (a, cdr) = do_demodulate(a);
         (a, List::Cons(Box::new(car), Box::new(cdr)))
     } else if prefix == "00" {
-        (&a[2..], List::Nil)
+        (a, List::Nil)
     } else {
         let sign = if prefix == "01" { 1 } else { -1 };
-        let a = &a[2..];
-        let mut len = 0;
-        for c in a.chars() {
-            if c == '0' {
-                break;
-            }
-            len += 1;
-        }
-        let a = &a[len + 1..];
-        len *= 4;
+        let len = a.find('0').unwrap();
         if len == 0 {
             return (a, List::Integer(0));
         }
+        let a = &a[len + 1..];
+        let len = len * 4;
         let res = i128::from_str_radix(&a[0..len], 2);
         let num = match res {
             Ok(b) => b,
@@ -39,8 +33,8 @@ fn do_demodulate(a: &str) -> (&str, List) {
                 panic!(e)
             }
         };
-        let a = &a[len..];
-        (a, List::Integer(sign * num))
+
+        (&a[len..], List::Integer(sign * num))
     }
 }
 
