1673673d7c95a0673e0e2ca354ddad33681c5744
Keiichi Watanabe <udon.watanabe@gmail.com>
2020-07-20 01:59:31 +0900

    Set initial params

--- app/ai.rs
+++ app/ai.rs
@@ -1,7 +1,14 @@
-use crate::protocol::{Command, GameInfo, GameState};
+use crate::protocol::*;
 
 pub trait AI {
     fn new() -> Self;
+
+    fn initial_params(info: &GameInfo) -> (u32, u32, u32, u32) {
+        match info.role {
+            Role::Attacker => (134, 64, 10, 1),
+            Role::Defender => (70, 64, 10, 1),
+        }
+    }
 
     fn main(&mut self, _info: &GameInfo, _state: &GameState) -> Vec<Command> {
         vec![]
