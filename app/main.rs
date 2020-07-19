914681b99cd8f54a906d03d310ac2525a45f488e
tikuta <tikuta@linux>
2020-07-19 13:44:33 +0900

    vendor

--- app/main.rs
+++ app/main.rs
@@ -1,7 +1,6 @@
-use http_body::Body as _;
-use hyper::{Body, Client, Method, Request, StatusCode};
 use std::env;
-use std::process;
+
+use ureq;
 
 mod sexpr2binary;
 use sexpr2binary::*;
@@ -10,46 +9,21 @@ async fn send(
     server_url: &str,
     request: &str,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client = Client::new();
-
     println!("request: {}", request);
 
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri(&format!(
-            "{}/aliens/send?apiKey=9ffa61129e0c45378b01b0817117622c",
-            server_url
-        ))
-        .body(Body::from(request.to_string()))?;
+    let url = format!(
+        "{}/aliens/send?apiKey=9ffa61129e0c45378b01b0817117622c",
+        server_url
+    );
 
-    match client.request(req).await {
-        Ok(mut res) => match res.status() {
-            StatusCode::OK => {
-                print!("Server response: ");
-                while let Some(chunk) = res.body_mut().data().await {
-                    match chunk {
-                        Ok(content) => println!("{:?}", content),
-                        Err(why) => println!("error reading body: {:?}", why),
-                    }
-                }
-            }
-            _ => {
-                println!("Unexpected server response:");
-                println!("HTTP code: {}", res.status());
-                print!("Response body: ");
-                while let Some(chunk) = res.body_mut().data().await {
-                    match chunk {
-                        Ok(content) => println!("{:?}", content),
-                        Err(why) => println!("error reading body: {:?}", why),
-                    }
-                }
-                process::exit(2);
-            }
-        },
-        Err(err) => {
-            println!("Unexpected server response:\n{}", err);
-            process::exit(1);
-        }
+    let resp = ureq::post(url).sned_string(&request);
+
+    if resp.ok() {
+        println!("success: {}", resp.into_string()?);
+    } else {
+        // This can include errors like failure to parse URL or connect timeout.
+        // They are treated as synthetic HTTP-level error statuses.
+        println!("error {}: {}", resp.status(), resp.into_string()?);
     }
 
     Ok(())
@@ -83,16 +57,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     // Join
     let request = make_join_request(player_key);
-    send(server_url, &request).await?;
+    send(server_url, &request);
 
     // Start
     let request = make_start_request(player_key);
-    send(server_url, &request).await?;
+    send(server_url, &request);
 
     // Game start
     loop {
         let request = make_game_request(player_key);
-        if let Err(_) = send(server_url, &request).await {
+        if let Err(_) = send(server_url, &request) {
             break;
         }
     }
