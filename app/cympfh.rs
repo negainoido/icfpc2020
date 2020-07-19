a2c71fee44dc6e25653442187359aefe670ba32d
cympfh <cympfh@gmail.com>
2020-07-20 05:09:01 +0900

    Avoid Detonate when defence

--- app/cympfh.rs
+++ app/cympfh.rs
@@ -194,6 +194,14 @@ impl AI for CympfhAI {
                 ship_id: ship_self.id,
                 target: y,
             }];
+        } else if ship_self.role == Role::Defender && close(&ship_self, &ship_enemy, 2) {
+            // 自爆回避
+            let g = Section::from(&ship_self.position).gravity();
+            let boost = (-g.0, -g.1);
+            return vec![Command::Accelerate {
+                ship_id: ship_self.id,
+                vector: boost,
+            }];
         } else if self.rand.gen::<i128>() % 4 == 0 {
             let boost = (
                 self.rand.gen::<i128>() % 3 - 1,
