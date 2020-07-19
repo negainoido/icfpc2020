bf1964237568e003ebf246070bed58a1b8053781
Takuto Ikuta <atetubou@gmail.com>
2020-07-19 23:15:12 +0900

    state parser (#178)
    * wip
    
    * wip2
    
    * wip3
    
    * test fail
    
    * pass join
    
    * faile start
    
    * defender
    
    * test pass
    
    * failed
    
    * failed
    
    * passed
    
    * x1
    
    * str
    
    * pass build
    
    * test pass
    
    Co-authored-by: tikuta <tikuta@linux>

--- app/main.rs
+++ app/main.rs
@@ -1,3 +1,5 @@
+mod protocol;
+
 use std::env;
 
 use ureq;
--- app/protocol.rs
+++ app/protocol.rs
@@ -1,25 +1,62 @@
+#![allow(unused)]
+
+use std::convert::TryFrom;
+
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+use icfpc2020::modulate::List;
+
 type ShipId = i128;
+
 type Coord = (i128, i128);
 
+#[derive(FromPrimitive, Debug, PartialEq, Eq)]
 enum GameStage {
     NotStarted = 0,
     Started = 1,
     Finished = 2,
 }
 
+#[derive(FromPrimitive, Debug, PartialEq, Eq)]
 enum Role {
     Attacker = 0,
     Defender = 1,
 }
 
+#[derive(Debug)]
 struct GameInfo {
-    x0: (),
+    x0: String,
     role: Role,
-    x2: (),
-    x3: (),
-    x4: (),
+    x2: String,
+    x3: String,
+    x4: String,
 }
 
+impl TryFrom<List> for GameInfo {
+    type Error = String;
+
+    fn try_from(l: List) -> Result<Self, Self::Error> {
+        let (x0, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (role, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (x2, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (x3, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (x4, l) = l.decompose().expect(&format!("not pair: {}", l));
+        if l.is_nil() {
+            Ok(GameInfo {
+                x0: format!("{}", x0),
+                role: FromPrimitive::from_i64(role.as_int().unwrap() as i64).unwrap(),
+                x2: format!("{}", x2),
+                x3: format!("{}", x3),
+                x4: format!("{}", x4),
+            })
+        } else {
+            Err(format!("GameInfo l is not nil: {}", l))
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 struct Ship {
     role: Role,
     id: ShipId,
@@ -27,20 +64,182 @@ struct Ship {
     velocity: Coord,
 }
 
+impl TryFrom<List> for Ship {
+    type Error = String;
+
+    fn try_from(l: List) -> Result<Self, Self::Error> {
+        let (role, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (shipid, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (position, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (velocity, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (_x4, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (_x5, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (_x6, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (_x7, l) = l.decompose().expect(&format!("not pair: {}", l));
+        if l.is_nil() {
+            dbg!(&position);
+            Ok(Ship {
+                role: FromPrimitive::from_i64(role.as_int().unwrap() as i64).unwrap(),
+                id: shipid.as_int().unwrap(),
+                position: position.as_coord().unwrap(),
+                velocity: velocity.as_coord().unwrap(),
+            })
+        } else {
+            Err(format!("Ship l is not nil: {}", l))
+        }
+    }
+}
+
+#[derive(Debug)]
 enum Command {
     Accelerate(ShipId, Coord),
     Detonate(ShipId),
     Shoot(ShipId, Coord),
 }
 
+#[derive(Default, Debug)]
 struct GameState {
     tick: i128,
-    x1: (),
-    ship_and_command: Vec<(Ship, Vec<Command>)>,
+    x1: String,
+    ship_and_commands: Vec<(Ship, Vec<Command>)>,
 }
 
+impl TryFrom<List> for GameState {
+    type Error = String;
+
+    fn try_from(l: List) -> Result<Self, Self::Error> {
+        if l.is_nil() {
+            return Ok(Default::default());
+        }
+        let (tick, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (x1, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (ship_and_commands, l) = l.decompose().expect(&format!("not pair: {}", l));
+
+        let mut cur_ship_and_commands = ship_and_commands;
+
+        let mut ship_commands = vec![];
+        loop {
+            let (cur, cdr) = cur_ship_and_commands.decompose().unwrap();
+
+            let (ship, _command) = cur.decompose().unwrap();
+            // TODO: parse _command;
+            ship_commands.push((Ship::try_from(*ship).unwrap(), vec![]));
+
+            if cdr.is_nil() {
+                break;
+            }
+            cur_ship_and_commands = cdr;
+        }
+
+        if l.is_nil() {
+            Ok(GameState {
+                tick: tick.as_int().unwrap(),
+                x1: format!("{}", x1),
+                ship_and_commands: ship_commands,
+            })
+        } else {
+            Err(format!("GameState l is not nil: {}", l))
+        }
+    }
+}
+
+#[derive(Debug)]
 struct GameResponse {
     stage: GameStage,
     info: GameInfo,
     state: GameState,
+}
+
+impl TryFrom<List> for GameResponse {
+    type Error = String;
+
+    fn try_from(l: List) -> Result<Self, Self::Error> {
+        let (status, l) = l.decompose().expect(&format!("not pair: {}", l));
+
+        if let Some(1) = status.as_int() {
+        } else {
+            return Err(format!("GameResponse status is not 1: {}", status));
+        }
+
+        let (stage, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (info, l) = l.decompose().expect(&format!("not pair: {}", l));
+        let (state, l) = l.decompose().expect(&format!("not pair: {}", l));
+        dbg!(&stage);
+        dbg!(&info);
+        dbg!(&state);
+
+        if l.is_nil() {
+            Ok(GameResponse {
+                stage: FromPrimitive::from_i64(stage.as_int().unwrap() as i64).unwrap(),
+                info: GameInfo::try_from(*info)?,
+                state: GameState::try_from(*state)?,
+            })
+        } else {
+            Err(format!("GameResponse l is not nil: {}", l))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    // use icfpc2020::modulate::*;
+
+    #[test]
+    fn join() {
+        // https://icfpc2020-api.testkontur.ru/logs/?logKey=6QcVllpAF%2BrDE%2BLh1mU37FG5ZyIlbx93Si%2BG%2BIls370%3D&apiKey=9ffa61129e0c45378b01b0817117622c
+        let join_resp = "110110000111010111101111000010000000011010111101111000100000000011011000011101110010000000011110111000010000110111010000000001111011000011101100001110110000111011000010000110000";
+        /*
+
+        (1 (0 ((256 (0 ((512 (1 (64 nil))) ((16 (128 nil)) ((1 (1 (1 (1 nil)))) nil))))) (nil nil))))
+         */
+        let l = List::demodulate(&join_resp).unwrap();
+
+        let game_resp = GameResponse::try_from(l).unwrap();
+        assert_eq!(game_resp.stage, GameStage::NotStarted);
+        let game_info = game_resp.info;
+        assert_eq!(game_info.role, Role::Attacker);
+    }
+
+    #[test]
+    fn start() {
+        // https://icfpc2020-api.testkontur.ru/logs?logKey=BvVVF1rAhqmTR9a0eTKCBMhNL4AZg7cor7gztcr%2B18k%3D&apiKey=9ffa61129e0c45378b01b0817117622c
+        let start_resp = "1101100001110110000111110111100001000000001101100001111101111000011100000011011000011101110010000000011110111000010000110111010000000001100001111010111101110000100001101110100000000011111111011000011101011111011000010111101100011000011110100101111011000011101100001110110000111011000010011010110111001000000110110000100110000111111010110110000111110111000010111011100011000011110100101111011000011101100001110110000111011000010011010110111001000000110110000100110000000000";
+        /*
+
+        (1 (1 ((256 (1 ((448 (1 (64 nil))) ((16 (128 nil)) (nil nil))))) ((0 ((16 (128 nil)) ((((1 (0 ((-23 -48) ((0 0) ((1 (1 (1 (1 nil)))) (0 (64 (1 nil)))))))) (nil nil)) (((0 (1 ((23 48) ((0 0) ((1 (1 (1 (1 nil)))) (0 (64 (1 nil)))))))) (nil nil)) nil)) nil))) nil))))
+         */
+        let l = List::demodulate(&start_resp).unwrap();
+
+        let game_resp = GameResponse::try_from(l).unwrap();
+        assert_eq!(game_resp.stage, GameStage::Started);
+        dbg!(&game_resp);
+        let game_info = game_resp.info;
+        assert_eq!(game_info.role, Role::Defender);
+        let game_state = game_resp.state;
+        assert_eq!(game_state.tick, 0);
+
+        let defender = &game_state.ship_and_commands[0].0;
+        assert_eq!(
+            &Ship {
+                role: Role::Defender,
+                id: 0,
+                position: (-23, -48),
+                velocity: (0, 0),
+            },
+            defender
+        );
+
+        let attacker = &game_state.ship_and_commands[1].0;
+
+        assert_eq!(
+            &Ship {
+                role: Role::Attacker,
+                id: 1,
+                position: (23, 48),
+                velocity: (0, 0),
+            },
+            attacker,
+        );
+    }
 }
