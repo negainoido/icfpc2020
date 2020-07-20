b101c376ad4b00cb723331fde511d696dc778e09
Takuto Ikuta <atetubou@gmail.com>
2020-07-20 21:48:38 +0900

    git squash commit for submissions/atetubou. (#246)
    * git squash commit for submissions/atetubou.
    
    26cf23e180687400c5838ff4dc0b572f3b422622
    is attacker
    
    224138bfd475c6ae3740638c10ade7b903edcebb
    wip
    
    f3ff6bf3e2b86af0366a5039b60c34b5f140cfe8
    passed
    
    a794a059c72cdd0871ef3cd2e2ee5a5d592c1f3d
    _
    
    64e16b6113f37d3a10ddf46fe4c270104fec38c8
    wip
    
    b13cc3e5bb258629fd0040cbb3a404b4456b007d
    gravity of
    
    b89d96f21c3ed5a2ce40d6e4b215f35dc36f13f1
    di
    
    83acc078c75f8feb31b9238f5649bde193c8325b
    cargo
    
    da6d16e3678e908dc93335771df2a1c01d216c42
    boots
    
    f88e0c97c9118425b23ad2363e808bef205f4fe3
    shipid2loopid
    
    41f62186f1bbd1c6d54fffa0966550e64f3cbe9b
    t
    
    a1433d3c8985d2d73dafcfca95a297f722eee00a
    passed build
    
    eb6ca965366f11247be7ec2e997379a144e3feea
    use ship id
    
    45a33ebe3ac8d703d350ce4410268b268659febd
    efficient clone
    
    77c632a6db059608b84b145c4c25fa3c902c8f19
    fix last vec
    
    b606c6ab754351f765645033c467d0547b2d5734
    param 8 81 out of 94
    
    7415f71b9024047790e5cf225a1aa9c9f7eef4ad
    14 68 unique out of 71
    
    * sub
    
    Co-authored-by: tikuta <tikuta@linux>

--- app/main.rs
+++ app/main.rs
@@ -25,7 +25,7 @@ use crate::protocol::ShipState;
  * Change this type to your AIs
  */
 type AttackerAI = CympfhAI;
-type DefenderAI = unagi_clone::UnagiCloneAI;
+type DefenderAI = new_unagi::UnagiCloneAI;
 
 fn send(server_url: &str, request: &str) -> Result<List, Box<dyn std::error::Error>> {
     println!("request: {}", request);
