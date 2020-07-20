3b15e090fce33941c69d1746db3c8a0b0ee67fe2
Takanori Hayashi <flowlight0@gmail.com>
2020-07-20 17:54:00 +0900

    Use unagi for defense

--- app/main.rs
+++ app/main.rs
@@ -24,7 +24,7 @@ use crate::protocol::ShipState;
  * Change this type to your AIs
  */
 type AttackerAI = CympfhAI;
-type DefenderAI = min_mutlti_ai::MinMultiAi;
+type DefenderAI = unagi_clone::UnagiCloneAI;
 
 fn send(server_url: &str, request: &str) -> Result<List, Box<dyn std::error::Error>> {
     println!("request: {}", request);
