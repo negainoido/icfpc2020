fa29b9aa5e6fd3a3ff2513fb513ab96826ebf59e
Keiichi Watanabe <udon.watanabe@gmail.com>
2020-07-20 15:30:40 +0900

    Parse AppliedCommands

--- app/protocol.rs
+++ app/protocol.rs
@@ -141,11 +141,81 @@ impl From<Command> for List {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum AppliedCommand {
+    Accelerated {
+        vector: Coord,
+    },
+    Detonated {
+        x1: i128,
+        x2: i128,
+    },
+    Shot {
+        target: Coord,
+        power: i128,
+        x4: i128,
+        x5: i128,
+    },
+    Unknown(List),
+}
+
+impl From<List> for AppliedCommand {
+    fn from(cmd: List) -> Self {
+        use AppliedCommand::*;
+        println!("AppliedCommand: {}", cmd);
+
+        let (typ, l) = cmd
+            .decompose()
+            .expect(&format!("failed getting command type: {}", cmd));
+
+        match typ.as_int().unwrap() {
+            0 => {
+                let (vector, l) = l.decompose().unwrap();
+                let vector = vector.as_coord().unwrap();
+                if !l.is_nil() {
+                    panic!("unexpected value for applied Accelerated command: {}", l);
+                }
+                Accelerated { vector }
+            }
+            1 => {
+                let (x1, l) = l.decompose().unwrap();
+                let (x2, l) = l.decompose().unwrap();
+                if !l.is_nil() {
+                    panic!("unexpected value for applied Detonated command: {}", l);
+                }
+                Detonated {
+                    x1: x1.as_int().unwrap(),
+                    x2: x2.as_int().unwrap(),
+                }
+            }
+            2 => {
+                let (target, l) = l.decompose().unwrap();
+                let (power, l) = l.decompose().unwrap();
+                let (x4, l) = l.decompose().unwrap();
+                let (x5, l) = l.decompose().unwrap();
+                if !l.is_nil() {
+                    panic!("unexpected value for applied Shot command: {}", l);
+                }
+                Shot {
+                    target: target.as_coord().unwrap(),
+                    power: power.as_int().unwrap(),
+                    x4: x4.as_int().unwrap(),
+                    x5: x5.as_int().unwrap(),
+                }
+            }
+            typ => {
+                println!("Unknown applied command: {}", typ);
+                Unknown(cmd)
+            }
+        }
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct GameState {
     pub tick: i128,
     pub x1: Vec<i128>,
-    pub ship_and_commands: Vec<(Ship, String)>,
+    pub ship_and_commands: Vec<(Ship, Vec<AppliedCommand>)>,
 }
 
 impl TryFrom<List> for GameState {
@@ -160,14 +230,30 @@ impl TryFrom<List> for GameState {
         let (ship_and_commands, l) = l.decompose().expect(&format!("not pair: {}", l));
 
         let mut cur_ship_and_commands = ship_and_commands;
-
         let mut ship_commands = vec![];
         loop {
             let (cur, cdr) = cur_ship_and_commands.decompose().unwrap();
 
-            let (ship, command) = cur.decompose().unwrap();
-            // TODO: parse _command;
-            ship_commands.push((Ship::try_from(*ship).unwrap(), format!("{}", command)));
+            let (ship, cur) = cur.decompose().unwrap();
+            let (commands, _cur) = cur.decompose().unwrap();
+            //assert_eq!(*cur, List::Nil);
+
+            println!("commands: {}", commands);
+
+            let mut cmd_vec: Vec<AppliedCommand> = Default::default();
+            let mut cur_cmds = commands;
+            loop {
+                if cur_cmds.is_nil() {
+                    break;
+                }
+                let (cmd, cmds) = cur_cmds.decompose().unwrap();
+                if cmd.is_nil() {
+                    break;
+                }
+                cmd_vec.push(AppliedCommand::from(*cmd));
+                cur_cmds = cmds;
+            }
+            ship_commands.push((Ship::try_from(*ship).unwrap(), cmd_vec));
 
             if cdr.is_nil() {
                 break;
@@ -298,6 +384,74 @@ mod test {
                 x7: 1,
             },
             attacker,
+        );
+    }
+
+    #[test]
+    fn shot_and_accelarated() {
+        // The last line of https://icfpc2020-api.testkontur.ru/logs?logKey=4KQ8iZkTIOaBSETrwSnX0OT7sVAGzyIOjrF5kQIT1xQ%3D&apiKey=9ffa61129e0c45378b01b0817117622c
+        let resp = "1101100001110110001011110111100001000000001101100001111101111000011100000011011000011101110010000000011110111000010000110111010000000001100001111011100010000011110111000010000110111010000000001111111101100001110101111101000110111000101111111101100011101001001111010110101101011010001101110010000001101110010000001101100001001100001111110101101100001111101110010101001011000100110111101100011011001111111011100111011011011100100000011011010101101100001001101110011111001101110100000001101100010001111110110001011111010001101110001011111101110010000001101110011000101101100100001111010111101010100001000000000000";
+        let l = List::demodulate(&resp).unwrap();
+        let game_resp = GameResponse::try_from(l).unwrap();
+        eprintln!("resp: {:?}", game_resp);
+    }
+
+    #[test]
+    fn unknown_applied_command() {
+        // https://icfpc2020-api.testkontur.ru/logs?logKey=5lyYxfZwONT37pqrSBiSVg1WqtjuZowdn8joV8A1YPo%3D&apiKey=9ffa61129e0c45378b01b0817117622c
+        let resp = "1101100001110110000111110111100001000000001101011110111100010000000001101100001110111001000000001111011100001000011011101000000000111101110100110001101011011010001101110011001000000111101100100111101110000100001101110100000000011111111011000011101011111011000011101011100011010111110110011101100101111101110100100101101011011010001101110011000110011011100001000011011101000000011011000100011111101100011111101011010110101101100001000000001111110101101100001111101110001100001011000100100111101001011110111010000010110111001000000110110101011011000010011010110111001000000110110000100111111010111110100001010000000111111011000011101100010111110110000111010111000110101111101100111011001011111010110101101011011000010011010110111010000000110110001000110000000000";
+        let l = List::demodulate(&resp).unwrap();
+        let game_resp = GameResponse::try_from(l).unwrap();
+        eprintln!("resp: {:?}", game_resp);
+    }
+
+    #[test]
+    fn detonated() {
+        // https://icfpcontest2020.github.io/#/visualize?game=06f74ed2-6d3e-4cce-b235-063d37eee065
+        // https://icfpc2020-api.testkontur.ru/logs?logKey=%2FyCYUNif1%2BjYAD73yrbDyLPjllShNz8eav70EWk%2F%2Bew%3D&apiKey=9ffa61129e0c45378b01b0817117622c
+        let resp = "11011000011101100010111101111000010000000011011000011111011110000111000000110110000111011100100000000111101110000100001101110100000000011000011110111000010110111101110000100001101110100000000011111111011000011101011110111000011000011100011011011110110010101011110101101011010110100011011100001100011011100100000011011000010011000011111101011011000011111011100001011101110001100001111010010111101011010110101101000110111001000100110111010000000110110001000111111010111110100001101000010011110110000111011110000101101101110111000100000000000000000";
+        let l = List::demodulate(&resp).unwrap();
+        let game_resp = GameResponse::try_from(l).unwrap();
+        eprintln!("resp: {:?}", game_resp);
+        let state = game_resp.state;
+
+        let (ship1, commands1) = &state.ship_and_commands[0];
+        assert_eq!(
+            *ship1,
+            Ship {
+                role: Role::Defender,
+                id: 0,
+                position: (24, 54),
+                velocity: (5, 0),
+                x4: vec![0, 0, 0, 0],
+                x5: 24,
+                x6: 64,
+                x7: 1
+            }
+        );
+        assert_eq!(*commands1, vec![]);
+
+        let (ship2, commands2) = &state.ship_and_commands[1];
+        assert_eq!(
+            *ship2,
+            Ship {
+                role: Role::Attacker,
+                id: 1,
+                position: (23, 48),
+                velocity: (0, 0),
+                x4: vec![0, 0, 0, 0],
+                x5: 68,
+                x6: 128,
+                x7: 2
+            }
+        );
+
+        assert_eq!(
+            *commands2,
+            vec![
+                AppliedCommand::Accelerated { vector: (-1, -1) },
+                AppliedCommand::Detonated { x1: 365, x2: 32 }
+            ]
         );
     }
 }
