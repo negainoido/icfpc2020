ef1d403bfb7d76de70e945168f7e0ac70e452348
Keiichi Watanabe <udon.watanabe@gmail.com>
2020-07-20 19:51:43 +0900

    Get join params as command args

--- test_app.sh
+++ test_app.sh
@@ -12,8 +12,8 @@ defender=$(echo "${resp}" | awk '{print $2}')
 echo $attacker $defender
 
 
-cargo run --bin app -- https://icfpc2020-api.testkontur.ru $attacker | tee attacker.log &
-cargo run --bin app -- https://icfpc2020-api.testkontur.ru $defender | tee defender.log &
+cargo run --bin app -- https://icfpc2020-api.testkontur.ru $attacker "1,1,1" | tee attacker.log &
+cargo run --bin app -- https://icfpc2020-api.testkontur.ru $defender "1,1,1" | tee defender.log &
 
 trap "kill 0" EXIT
 
